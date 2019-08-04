03b6c829dc7900256081ba24ad4dd8ce569383b0
Jing Yang <jingyang_carl@qq.com>
2019-08-04 15:12:33 -0700

    Add comments

--- Code/Tutorial1/Tutorial1/Widget.cpp
+++ Code/Tutorial1/Tutorial1/Widget.cpp
@@ -1,12 +1,32 @@
 #include "Widget.h"
 
+/*
+Description:
+	This function is a constructor;
+Input:
+	@ QWidget* parent:
+*/
 Widget::Widget(QWidget* parent) :
 	QOpenGLWidget(parent), texture(0), indexBuffer(QOpenGLBuffer::IndexBuffer) {
 }
 
+/*
+Description:
+	This function is a destructor;
+Input:
+	@ void patameter: void;
+*/
 Widget::~Widget() {
 }
 
+/*
+Description:
+	This function is used to initialize OpenGL state machine, and initialize shaders ,objects and etc.;
+Input:
+	@ void parameter: void;
+Output:
+	@ void returnValue: void;
+*/
 void Widget::initializeGL() {
 	// clear the screen with black
 	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
@@ -19,6 +39,15 @@ void Widget::initializeGL() {
 	initCube(1.0);
 }
 
+/*
+Description:
+	This function is used to deal with resive event;
+Input:
+	@ int width: window width after resize event;
+	@ int height: window height after resize event;
+Output:
+	@ void returnValue: void;
+*/
 void Widget::resizeGL(int width, int height) {
 	float aspect = width / (float)height;
 
@@ -26,6 +55,14 @@ void Widget::resizeGL(int width, int height) {
 	pMatrix.perspective(45, aspect, 0.1f, 10.0f);
 }
 
+/*
+Description:
+	This function is used to set parameters for the vertex shader, fragment shader and etc. and draw other objects;
+Input:
+	@ void parameter: void;
+Output:
+	@ void returnValue: void;
+*/
 void Widget::paintGL() {
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
@@ -61,6 +98,14 @@ void Widget::paintGL() {
 
 }
 
+/*
+Description:
+	This function is used to initialize shaders for skybox and objects;
+Input:
+	@ void parameter: void;
+Output:
+	@ void returnValue: void;
+*/
 void Widget::initShaders() {
 	if (!shaderProgram.addShaderFromSourceFile(QOpenGLShader::Vertex, "./VertexShader.vsh"))
 		close();
@@ -68,9 +113,16 @@ void Widget::initShaders() {
 		close();
 	if (!shaderProgram.link())
 		close();
-
 }
 
+/*
+Description:
+	This function is used to load graphics data for a cube, including vertex data and index data;
+Input:
+	@ int width: the width of the cube;
+Output:
+	@ void returnValue: void;
+*/
 void Widget::initCube(float width) {
 	QVector<Vertex> vertices;
 	vertices <<
